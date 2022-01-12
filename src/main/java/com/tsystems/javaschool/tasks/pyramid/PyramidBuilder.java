92626606842157837e853c2d43acec2d1421e314
dimastasky <dimastasky@gmail.com>
2022-01-12 16:52:11 +0500

    Pyramid builder task completed and successfully tested

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -14,8 +15,42 @@ public class PyramidBuilder {
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
         // TODO : Implement your solution here
-        return new int[0][0];
+        checkInputNumbers(inputNumbers);
+        Collections.sort(inputNumbers);
+        int pyramidHeight = getHeight(inputNumbers.size());
+        int pyramidWidth = getWidth(pyramidHeight);
+        int[][] pyramid = new int[pyramidHeight][pyramidWidth];
+
+        int listIndex = 0;
+        for (int currentHeight = 0; currentHeight < pyramidHeight; currentHeight++) {
+            for (int currentWidth = 0; currentWidth < currentHeight + 1; currentWidth++) {
+                pyramid[currentHeight][pyramidWidth / 2 - currentHeight + currentWidth * 2] = inputNumbers.get(listIndex);
+                listIndex++;
+            }
+        }
+        return pyramid;
     }
 
+    //Check Linked List with input numbers for minimal and maximal size range and if contain null elements
+    private static void checkInputNumbers(List<Integer> inputNumbers) {
+        if (inputNumbers.size() < 3 ||
+                inputNumbers.size() >= Integer.MAX_VALUE-1 ||
+                inputNumbers.contains(null)) throw new CannotBuildPyramidException();
+    }
 
+    //Get height of Pyramid using inputNumbers size
+    private static int getHeight(int listSize) {
+        int height = 0;
+        for (int elementsCount = listSize; elementsCount > 0; elementsCount -= height) {
+            //the number of remaining elements cannot be lower than the height of the pyramid
+            if (elementsCount < height) throw new CannotBuildPyramidException();
+            height++;
+        }
+        return height;
+    }
+
+    //Get width of Pyramid using height of pyramid
+    private static int getWidth(int height) {
+        return (height * 2) - 1;
+    }
 }
