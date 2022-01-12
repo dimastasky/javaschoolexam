6a0ef06625d5c89187928c4ecdbb951e2c0c08e7
dimastasky <dimastasky@gmail.com>
2022-01-12 18:35:01 +0500

    Subsequence task completed and successfully tested

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -14,7 +14,18 @@ public class Subsequence {
      */
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+            boolean isBuildable = false;
+            if (x == null || y == null) throw new IllegalArgumentException();
+            if (x.isEmpty()) {
+                return true; //TRUE When List X is empty, it's always subsequence of List Y (even if List Y is emt too)
+            } else if (y.isEmpty() || y.size() < x.size()) {
+                return false; //FALSE When List X contains elements and List Y is Empty or Y size less than X size
+            } else {
+                for(int iX = 0, iY = 0; iX < x.size(); iY++) {
+                    if (iY > y.size()-1) return false; //TRUE When List Y doesn't contain any element from List X
+                    if (x.get(iX).equals(y.get(iY))) iX++;
+                }
+            }
+        return true; //TRUE When List X is subsequence of List Y
     }
 }
